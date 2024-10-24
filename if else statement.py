ac14c3923e0211666b5d3e723d128f7a9d3d14af
MeghanaLavangu <mlavangu@gitam.in>
2024-10-25 01:11:56 +0530

    Add files via upload

---
+++ if else statement.py
@@ -0,0 +1,11 @@
+mark = 66
+
+if(mark >= 90):
+    grade = "A"
+elif(mark >= 80 and mark <= 90):
+     grade = "B"
+elif(mark >= 70 and mark <= 80):
+     grade = "C"
+else:
+     grade = "D"
+print("grade of the student",grade)
